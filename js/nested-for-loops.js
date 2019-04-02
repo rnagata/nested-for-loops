bfeb63fae5947b0aac19a817cedc1e45f4fc266d
Ronald J Nagata Jr <junzo@Ronalds-MBP.home>
2019-04-02 12:33:03 -1000

    First Solution

--- js/nested-for-loops.js
+++ js/nested-for-loops.js
@@ -6,9 +6,31 @@
  */
 function nestedForLoops (depth, width = depth) {
   let result = "\n";
-
+  
   // Write code here
-
+  if (!depth || !width ){
+    return "";
+  }
+  if (typeof depth !== 'number'){
+    return "";
+  }
+  if (typeof width !== 'number'){
+    return "";
+  }
+  
+  for (let depthCount = 0; depthCount < depth; depthCount++){
+    for (let widthCount = 0; widthCount < width; widthCount++){
+      result = result.concat('{x:', widthCount.toString(), ', ');
+      result = result.concat('y:', depthCount.toString(), '}');
+      if (widthCount < width - 1){
+        result = result.concat(", ");
+      }
+      if (widthCount === width - 1){
+        result = result.concat('\n');
+      }
+    }
+  }
+  return result;
 };
 
 // To see your console output outside the tests add function calls here.
